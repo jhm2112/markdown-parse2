402eabb038b95cbd3a02b87dfd7d892b00785524
jhm2112 <79220026+jhm2112@users.noreply.github.com>
2022-02-25 14:01:31 -0800

    lab8 updates

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
@@ -21,18 +22,43 @@ public class MarkdownParseTest {
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
-    @Test
-    public void testMissingCloseParen() {
-        String contents= "[link title](a.com";
-        List<String> expect = List.of();
-        assertEquals(MarkdownParse.getLinks(contents), expect);
-    }
 
     @Test
     public void testSpaceAroundLink() {
         String contents= "[link title](   a.com   )";
         List<String> expect = List.of("a.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("snippet1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("`google.com", links.get(0));
+        assertEquals("google.com", links.get(1));            
+        assertEquals("ucsd.edu", links.get(2));
+        assertEquals(3, links.size());
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("snippet2.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("a.com", links.get(0));
+        assertEquals("a.com(())", links.get(1));
+        assertEquals("example.com", links.get(2));
+        assertEquals(3, links.size());
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("snippet3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(" https://ucsd-cse15l-w22.github.io/", links.get(0));
+        assertEquals(1, links.size());
     }
 
 }
